fb4a3a9d6bb562f116abc742621bf306a7e09870
wuttichaikihart-afk <it.kihart@gmail.com>
2026-07-23 23:35:16 +0700

    Fix Prisma v7 requirement for pg driver adapter

--- src/services/db.js
+++ src/services/db.js
@@ -1,12 +1,18 @@
 const { PrismaClient } = require('@prisma/client');
+const { Pool } = require('pg');
+const { PrismaPg } = require('@prisma/adapter-pg');
+
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
 
 let prisma;
 
 if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
+  prisma = new PrismaClient({ adapter });
 } else {
   if (!global.prisma) {
-    global.prisma = new PrismaClient();
+    global.prisma = new PrismaClient({ adapter });
   }
   prisma = global.prisma;
 }
